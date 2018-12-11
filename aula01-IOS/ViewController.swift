cdf6c9fae9a67459d541debc663a4c795884bd1c
aluno <aluno@LABMAC03.local>
2018-12-10 21:45:20 -0300

    aula 1.

--- aula01-IOS/ViewController.swift
+++ aula01-IOS/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flips: UILabel!
+    
+    var flipCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func flipCard2(_ sender: UIButton) {
+        flipCount += 1
+        flips.text = "Flips: \(flipCount)"
+        if sender.currentTitle == "😡" {
+            sender.setTitle("", for: UIControl.State.normal)
+        } else {
+            sender.setTitle("😡", for: UIControl.State.normal)
+        }
+    }
+    
+    @IBAction func flipCard(_ sender: UIButton) {
+        flipCount += 1
+        flips.text = "Flips: \(flipCount)"
+        if sender.currentTitle == "🙃" {
+            sender.setTitle("", for: UIControl.State.normal)
+        } else {
+            sender.setTitle("🙃", for: UIControl.State.normal)
+        }
+    }
 }
 
